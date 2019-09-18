6701c436b86b2bea658c41c30658ff15fcad6fb0
Shantanu Verma <shantanuverma12@gmail.com>
2019-09-18 22:22:12 +0530

    Changes

--- virtualDisk.py
+++ virtualDisk.py
@@ -1,4 +1,5 @@
 import random
+from random import randint
 from physicalDisk import *
 from copy import deepcopy
 
@@ -45,73 +46,80 @@ class DiskManager:
     def createFragment(self, id, num_blocks):
         if(id not in self.diskMap):
             self.diskMap[id] = Disk(id, num_blocks)
+        l = []
+        for n in range(len(self.unoccupiedfragments)):
+            i = self.unoccupiedfragments[n]
+            if(i.num >= num_blocks):
+                l.append((n, i));
         disk = self.diskMap[id]
-
-        l = [(n, i) for n, i in enumerate(self.unoccupiedfragments) if i.num >= num_blocks]
-
         if(len(l) == 0):
-            p = Fragment(self.unoccupiedfragments[-1].block, self.unoccupiedfragments[-1].num)
-            (disk.patches).append(p)
+            bl, nu = self.unoccupiedfragments[-1].block, self.unoccupiedfragments[-1].num
+            disk.fragments.append(Fragment(bl, nu))
             self.unoccupiedfragments.pop()
-            self.usedBlocks += p.num
-            self.createFragment(id, num_blocks - p.num)
+            self.usedBlocks += nu
+            self.createFragment(id, num_blocks - nu)
         else:
             index = l[0][0]
-            fragblocknum = l[0][1].block
-            fragnum = l[0][1].num
-            (disk.patches).append(Fragment(fragblocknum, num_blocks))
-            if(fragnum == num_blocks):
+            disk.fragments.append(Fragment(l[0][1].block, num_blocks))
+            if(l[0][1].num == num_blocks):
                 self.unoccupiedfragments.pop(index)
             else:
-                currval = fragnum - num_blocks
+                currval = l[0][1].num - num_blocks
                 while((index > 0) and self.unoccupiedfragments[index-1].num > currval):
-                    self.unoccupiedfragments[index].block = self.unoccupiedfragments[index - 1].block
-                    self.unoccupiedfragments[index].num = self.unoccupiedfragments[index - 1].num
                     index -= 1
-                self.unoccupiedfragments[index].block = fragblocknum + num_blocks
+                    nuuu, boo = self.unoccupiedfragments[index - 1].num, self.unoccupiedfragments[index - 1].block
+                    self.unoccupiedfragments[index].num = nuuu
+                    self.unoccupiedfragments[index].block = boo
+                self.unoccupiedfragments[index].block = l[0][1].block + num_blocks
                 self.unoccupiedfragments[index].num = currval
             self.usedBlocks += num_blocks
 
     def getVirtualDiskNo(self, diskpatches, block_no):
-        total_blocks = 0
         i = 0
-        while(diskpatches[i].num + total_blocks < block_no + 1):
-            total_blocks += diskpatches[i].num
+        total_blocks = 0
+        while(diskpatches[i].num + total_blocks <= block_no):
             i += 1
+            total_blocks += diskpatches[i].num
         return diskpatches[i].block + block_no - total_blocks
 
     def readDiskBlock(self, id, block_no):
         if(id not in self.diskMap):
             raise Exception('Invalid Disk Id')
         disk = self.diskMap[id]
-        if(disk.numBlocks < block_no + 1):
+        if(disk.numberOfBlocks < block_no + 1):
             raise Exception('Invalid Block Number')
         print("Reading from disk" + str(id))
-        disk.commandList.append(("readDiskBlock", block_no))
-        vdnum = self.getVirtualDiskNo(disk.patches, block_no)
+        disk.commands.append(("readDiskBlock", block_no))
+        vdnum = self.getVirtualDiskNo(disk.fragments, block_no)
         return self.vd.read(vdnum)
 
     def writeDiskBlock(self, id, block_no, data):
         if(id not in self.diskMap):
             raise Exception('Invalid Disk Id')
         disk = self.diskMap[id]
-        if(disk.numBlocks < block_no + 1):
+        if(disk.numberOfBlocks < block_no + 1):
             raise Exception('Invalid Block Number')
         print("Writing to disk" + str(id))
-        disk.commandList.append(("writeDiskBlock", block_no, data))
-        vdnum = self.getVirtualDiskNo(disk.patches, block_no)
+        disk.commands.append(("writeDiskBlock", block_no, data))
+        vdnum = self.getVirtualDiskNo(disk.fragments, block_no)
         self.vd.write(vdnum, data)
         print("Written data to disk")
 
     def deleteDisk(self, id):
         if(id not in self.diskMap):
-            raise Exception('Invalid Disk Id')
+            print('Invalid Disk Id')
+            return;
         disk = self.diskMap[id]
-        unoccupied = self.unoccupiedfragments + disk.patches
-        unocc_sorted_ind = sorted(unoccupied, key = lambda x : x.block)
+        self.usedBlocks -= disk.numberOfBlocks
+        unoccupied = self.unoccupiedfragments + disk.fragments
+        def sortArray(l, b = 0):
+            if(b == 0):
+                return sorted(l, key = lambda x : x.block)
+            else:
+                return sorted(l, key = lambda x : x.num)
+        unocc_sorted_ind = sortArray(unoccupied)
         unocc_new = self.mergeFragments(unocc_sorted_ind)
-        self.unoccupiedfragments = sorted(unocc_new, key = lambda x : x.num)
-        self.usedBlocks -= disk.numBlocks
+        self.unoccupiedfragments = sortArray(unocc_new, 1)
         self.diskMap.pop(id)
         print('Deleted the disk with id ' + str(id))
 
@@ -119,26 +127,29 @@ class DiskManager:
         if(id not in self.diskMap):
             raise Exception('Invalid Disk Id')
         disk = self.diskMap[id]
-        disk.commandList.append(("readDiskBlock", block))
-        if(disk.numBlocks < block + 1):
-            raise Exception('Invalid Block Number')
-        if(random.randint(1, 200) < 20):
+        disk.commands.append(("readDiskBlock", block))
+        if(disk.numberOfBlocks < block + 1):
+            print("Cannot make replica")
+            return;
+        random.seed(0)
+        if(randint(1, 200) > 180):
             print("Bad Block Encountered!")
             if(len(self.unoccupiedfragments) == 0):
-                raise Exception("Replica error")
+                print("Replica error")
+                return;
             else:
                 #Creating a new block to replace a bad block
                 newReplicaBlockn = self.unoccupiedfragments[0].block
-                if(self.unoccupiedfragments[0].num == 1):
-                    self.unoccupiedfragments.pop(0)
-                else:
-                    self.unoccupiedfragments[0].num -= 1
+                if(self.unoccupiedfragments[0].num != 1):
                     self.unoccupiedfragments[0].block += 1
+                    self.unoccupiedfragments[0].num -= 1
+                else:
+                    self.unoccupiedfragments.pop(0)
                 self.usedBlocks += 1
 
                 # Getting the block replica details
                 new_patches = []
-                virt_orig = self.getVirtualDiskNo(disk.patches, block)
+                virt_orig = self.getVirtualDiskNo(disk.fragments, block)
                 virt_replica = self.vd.getBlockReplica(virt_orig)
                 virt_new_replica = newReplicaBlockn
 
@@ -151,49 +162,39 @@ class DiskManager:
                 ans = self.vd.read(virt_replica)
                 self.vd.write(virt_new_replica, ans)
 
-                neworig = Fragment(virt_replica, 1)
-                newReplica = Fragment(virt_new_replica, 1)
+                for p in disk.fragments:
+                    if((virt_orig < p.block or virt_orig >= (p.block + p.num - 1)) and (p.block - 1) < virt_replica and (virt_replica - 1) <= (p.block + p.num)):
+                        if(virt_replica >= p.block - 1):
+                            new_patches.append(Fragment(p.block, virt_replica - p.block))
+                        new_patches.append(Fragment(virt_new_replica, 1))
+                        if((virt_replica + 1) < (p.block + p.num)):
+                            new_patches.appned(Fragment(virt_replica + 1, p.num  - (virt_replica - p.block + 1)))
 
-                for p in disk.patches:
-                    if((virt_orig < p.block or virt_replica >= (p.block + p.num)) and (virt_orig < p.block or virt_orig >= (p.block + p.num))):
+                    elif(((virt_orig - 1) <= p.block or virt_orig >= (p.block + p.num)) and (virt_orig - 1 <= p.block or virt_replica >= (p.block + p.num))):
                         new_patches.append(p)
 
-                    elif((virt_orig < p.block or virt_orig > (p.block + p.num)) and p.block <= virt_replica and virt_replica < (p.block + p.num)):
-                        if(virt_replica > p.block):
-                            left_p = Fragment(p.block, virt_replica - p.block)
-                            new_patches.append(left_p)
-                        new_patches.append(newReplica)
-                        if(virt_replica < (p.block + p.num - 1)):
-                            right_p = Fragment(virt_replica + 1, p.num  - (virt_replica - p.block + 1))
-                            new_patches.appned(right_p)
-
-                    elif((virt_replica > (p.block + p.num) or virt_replica < p.block) and p.block <= virt_orig and virt_orig < (p.block + p.num)):
+                    elif(p.block <= virt_orig and virt_orig < (p.block + p.num) and (virt_replica > (p.block + p.num) or virt_replica < p.block)):
                         if(virt_orig > p.block):
-                            left_p = Fragment(p.block, virt_orig - p.block)
-                            new_patches.append(left_p)
-                        new_patches.append(neworig)
-                        if(virt_orig < (p.block + p.num + 1)):
-                            right_p = Fragment(virt_orig + 1, p.num - (virt_orig - p.block + 1))
-                            new_patches.append(right_p)
+                            new_patches.append(Fragment(p.block, virt_orig - p.block))
+                        new_patches.append(Fragment(virt_replica, 1))
+                        if(virt_orig <= (p.block + p.num)):
+                            new_patches.append(Fragment(virt_orig + 1, p.num - (virt_orig - p.block + 1)))
 
                     else:
-                        if(virt_orig > p.block):
-                            left_p = Fragment(p.block, virt_orig - p.block)
-                            new_patches.append(left_p)
-                        new_patches.append(neworig)
-                        if(virt_orig < virt_replica - 1):
-                            mid_p = Fragment(virt_orig + 1, virt_replica  - virt_orig - 1)
-                            new_patches.append(mid_p)
-                        new_patches.append(newReplica)
-                        if(virt_replica < (p.block + p.num - 1)):
-                            right_p = Fragment(virt_replica + 1, p.num - (virt_replica - p.block + 1))
-                            new_patches.append(right_p)
+                        if(virt_orig >= p.block - 1):
+                            new_patches.append(Fragment(p.block, virt_orig - p.block))
+                        new_patches.append(Fragment(virt_replica, 1))
+                        if(virt_orig <= virt_replica - 2):
+                            new_patches.append(Fragment(virt_orig + 1, virt_replica  - virt_orig - 1))
+                        new_patches.append(Fragment(virt_new_replica, 1))
+                        if(virt_replica + 1 < (p.block + p.num)):
+                            new_patches.append(Fragment(virt_replica + 1, p.num - (virt_replica - p.block + 1)))
 
-                    disk.patches = self.mergeFragments(new_patches)
+                    disk.fragments = self.mergeFragments(new_patches)
 
         else:
             print("Reading from the disk" + str(id))
-            vdnum = self.getVirtualDiskNo(disk.patches, block)
+            vdnum = self.getVirtualDiskNo(disk.fragments, block)
             ans = self.vd.read(vdnum)
         return ans
 
@@ -201,17 +202,17 @@ class DiskManager:
         if(id not in self.diskMap):
             raise Exception('Invalid Disk Id')
         disk = self.diskMap[id]
-        if(disk.numBlocks < block + 1):
+        if(disk.numberOfBlocks < block + 1):
             raise Exception('Invalid Block Number')
         # Writing data in primary block
-        disk.commandList.append(("writeDiskBlock", block, data))
-        vdnum = self.getVirtualDiskNo(disk.patches, block)
+        disk.commands.append(("writeDiskBlock", block, data))
+        vdnum = self.getVirtualDiskNo(disk.fragments, block)
         self.vd.write(vdnum, data)
 
         # Creating a replica
-        delta = disk.numBlocks // 2
+        delta = disk.numberOfBlocks // 2
         replica_disk = (block + delta) if block < delta else block - delta
-        virtual_replica_block = self.getVirtualDiskNo(disk.patches, replica_disk)
+        virtual_replica_block = self.getVirtualDiskNo(disk.fragments, replica_disk)
         curr_replica = self.vd.getBlockReplica(vdnum)
         if(curr_replica == -1 or curr_replica != virtual_replica_block):
             self.vd.setBlockReplica(vdnum, virtual_replica_block)
@@ -225,29 +226,29 @@ class DiskManager:
         if(id not in self.diskMap):
             raise Exception('Invalid Disk Id')
         disk = self.diskMap[id]
-        disk.checkPointMap.append(len(disk.commandList))
-        return len(disk.checkPointMap)-1
+        disk.checkPoints.append(len(disk.commands))
+        return len(disk.checkPoints)-1
 
     def rollback(self,disk_id,checkpoint_id):
         if(disk_id not in self.diskMap):
             raise('Invalid Disk Id')
         disk = self.diskMap[disk_id]
-        checkpoints = disk.checkPointMap[0:checkpoint_id]
-        commands = disk.commandList[0:disk.checkPointMap[checkpoint_id]]
+        checkpoints = disk.checkPoints[0 :checkpoint_id]
+        commands = disk.commands[0 : disk.checkPoints[checkpoint_id]]
 
         self.deleteDisk(disk_id)
 
         for command in commands:
-            if command[0] == "createDisk":
-                self.createDisk(command[1],command[2])
+            if(command[0] == "createDisk"):
+                self.createDisk(command[1], command[2])
                 disk = self.diskMap[disk_id]
-            elif command[0] == "readDiskBlock":
-                x = self.readDiskBlock(disk_id,command[1])
-            else:
-                self.writeDiskBlock(disk_id,command[1],command[2])
+            elif(command[0] == "readDiskBlock"):
+                x = self.readDiskBlock(disk_id, command[1])
+            elif(command[0] == "writeDiskBlock"):
+                self.writeDiskBlock(disk_id, command[1], command[2])
 
-        disk.checkPointMap = checkpoints
-        disk.commandList = commands
+        disk.checkPoints = deepcopy(checkpoints)
+        disk.commands = deepcopy(commands)
 
 
 if __name__ == '__main__':
